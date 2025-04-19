4bf311223417f3102923fc6f1fb58eeab7863790
oangia <hqnhatdn@gmail.com>
2025-04-20 00:42:07 +0700

    Create __init__.py

---
+++ yt/__init__.py
@@ -0,0 +1,13 @@
+def download_channel(channel):
+    channel_url = 'https://www.youtube.com/@' + channel +'/videos'
+    
+    ydl_opts = {
+        'extract_flat': True,
+        'quiet': True,
+        'skip_download': True,
+    }
+    
+    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+        info = ydl.extract_info(channel_url, download=False)
+    
+    return info
